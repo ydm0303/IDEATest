7aa71b60b34a5ce2f7fb5a5411ad302e0528b8c3
ydm <ydm@123.com>
2021-07-22 20:35:50 +0800

    IDEA  test_01

---
+++ src/Java.java
@@ -0,0 +1,13 @@
+import javafx.scene.chart.PieChart;
+
+/**
+ * @ClassName Java
+ * @Description TODO
+ * @Author YDM
+ * @Date 2021/7/22 19:10
+ **/
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("GitHub ¹þ¹þ£¡");
+    }
+}
